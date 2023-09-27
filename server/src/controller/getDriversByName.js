8822b4ee328ad5069617e625971e3bbfd9c8d7d1
AgustinSolaberrieta <agustinasolaberrieta08@gmail.com>
2023-09-27 11:01:58 -0300

    pepe2

--- server/src/controller/getDriversByName.js
+++ server/src/controller/getDriversByName.js
@@ -1,3 +1,7 @@
+// // Esta ruta debe obtener los primeros 15 drivers que se encuentren con la palabra recibida por query.
+// // Debe poder buscarlo independientemente de mayúsculas o minúsculas.
+// // Si no existe el driver, debe mostrar un mensaje adecuado.
+// // Debe buscar tanto los de la API como los de la base de dato
 // const axios = require('axios');
 // const { Driver } = require('../db.js');
 
@@ -6,9 +10,9 @@
 // const getDriversByName = async (req, res) => {
 
 //   try {
-//     const { name } = req.query.toLowerCase();
+//     const { name } = req.query.name.toLowerCase();
 //       const dbid = await Driver.findOne({
-//         where: {name}
+//         where: {'$name.forename$':name}
 //       });
 
 //      if(dbid){
@@ -40,37 +44,43 @@
 
 // const API_URL = 'http://localhost:5000/drivers';
 
+
 // const getDriversByName = async (req, res) => {
 //   try {
+//     // Obtén el nombre de la consulta y conviértelo a minúsculas
 //     const { name } = req.query;
-//     const lowerCaseName = name.toLowerCase();
+//     const nameLower = name.toLowerCase();
 
-//     // Buscar en la base de datos
+//     // Busca en la base de datos
 //     const dbDriver = await Driver.findOne({
 //       where: {
-//         '$name.forename$': lowerCaseName,
+//         '$name.forename$': name,
 //       },
 //     });
 
 //     if (dbDriver) {
-//       res.status(200).json(dbDriver);
-//     } else {
-//       // Si no se encuentra en la base de datos, buscar en la API externa
-//       const response = await axios.get(API_URL, {
-//         params: {
-//           'name.forename': lowerCaseName,
-//           limit: 1,
-//         },
-//       });
-
-//       if (response.data) {
-//         res.status(200).json(response.data);
-//       } else {
-//         res.status(404).json({ error: 'Conductor no encontrado' });
-//       }
+//     //   Si se encuentra en la base de datos, devuelve el resultado
+//       return res.status(200).json(dbDriver);
 //     }
+
+//     // Si no se encuentra en la base de datos, busca en la API externa
+//     const response = await axios.get(`${URL}/${name}`, {
+//       params: {
+//         'name.forename': nameLower,
+//         limit: 15,
+//       },
+//     });
+
+//     if (response.data.length > 0) {
+//     //   Si se encuentra en la API externa, devuelve el resultado
+//       return res.status(200).json({mensaaje: "hahhah"});
+//     }
+
+//     // Si no se encuentra en ningún lugar, devuelve un mensaje de error
+//     return res.status(404).json({ error: 'Conductor no encontrado' });
 //   } catch (error) {
-//     res.status(500).json({ error: 'Error interno del servidor' });
+//     // Maneja los errores
+//     return res.status(500).json({ error: 'Error interno del servidor' });
 //   }
 // };
 
--- server/src/controller/postDivers.js
+++ server/src/controller/postDivers.js
@@ -1,122 +1,62 @@
-// // 📍 POST | /drivers
-// // Esta ruta recibirá todos los datos necesarios para crear un driver ++
-// //y relacionarlo con sus teams solicitados.
-// // Toda la información debe ser recibida por body.++
-// // Debe crear un driver en la base de datos,
-// // y este debe estar relacionado con sus teams indicados (al menos uno)
+// // // 📍 POST | /drivers
+// // // Esta ruta recibirá todos los datos necesarios para crear un driver ++
+// // //y relacionarlo con sus teams solicitados.
+// // // Toda la información debe ser recibida por body.++
+// // // Debe crear un driver en la base de datos,
+// // // y este debe estar relacionado con sus teams indicados (al menos uno)
 
-// // const {Teams, Driver, Drivers_Teams} = require("../db")
-
-
-// // const postDrivers = async (req, res) => {
-
-// //     try{
-// //     const {name , surname , description , image , nationality , dob, teams } = req.body
-
-// //     if (!name || !surname || !description || !image || !nationality || !dob || !teams) {
-// //         res.status(404).send("Faltan datos para crear un driver")
-// //         return
-// //     }
-// //      const [newDriver, created]= await Driver.create({
-// //         where:{
-// //             name,
-// //             surname , 
-// //             description ,
-// //             image , 
-// //             nationality , 
-// //             dob},
-        
-// //         })
-
-// //         if (!created) {
-// //             res.status(400).send("No se puede crear el nuevo Driver")
-// //             return
-// //         }
-
-// //         for (let teamName of teams) {
-// //             const [teamInstance] = await Teams.findOrCreate({
-// //                 where: { name: teamName }
-// //             });
-            
-// //             await Drivers_Teams.create({
-// //                 teamId: newDriver.id,
-// //                 typeId: teamInstance.id
-// //             });
-// //         }
-        
-// //     res.status(200).json(newDriver)
-
-// //  }catch (error){
-// //     console.log(error.message);
-// //     res.status(500).json({menssaje: "hplll"})
-// //  }
+const { Teams, Driver, Drivers_Teams } = require("../db");
 
 
-// // }
+const postDrivers = async (req, res) => {
+    try {
+      const { name, surname, description, image, nationality, dob, teams } = req.body;
+  
+      if (!name || !surname || !description || !image || !nationality || !dob || !teams) {
+        return res.status(404).json({ error: "Faltan datos para crear un driver" });
+      }
+  
+      // Crea el nuevo conductor en la base de datos
+      const newDriver = await Driver.create({
+        name,
+        surname,
+        description,
+        image,
+        nationality,
+        dob,
+      });
+  
+      // Verifica si newDriver se creó con éxito
+      if (!newDriver) {
+        res.status(400).send("No se puede crear el nuevo Driver");
+        return;
+      }
+  
+      // Ahora, crea las relaciones con los equipos
+      for (let teamName of teams) {
+        const [teamInstance] = await Teams.findOrCreate({
+          where: { name: teamName },
+        });
+  
+        // Verifica si teamInstance se creó con éxito
+        if (!teamInstance) {
+          res.status(400).send(`No se puede encontrar/crear el equipo: ${teamName}`);
+          return;
+        }
+  
+        // Crea la relación en la tabla Drivers_Teams
+        await newDriver.addTeams(teamInstance); // Utiliza el nombre 'Teams' que definiste en db.js
+      }
+  
+      res.status(200).json(newDriver);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ mensaje: "Hubo un error en el servidor" });
+    }
+  };
+  
+  module.exports = {
+    postDrivers,
+  };
 
-// // module.exports={
-// //     postDrivers,
-// // }
-
-
-
-// const { Teams, Driver, Drivers_Teams } = require("../db");
-
-// const postDrivers = async (req, res) => {
-//   try {
-//     const { name, surname, description, image, nationality, dob, teams } = req.body;
-
-//     if (!name || !surname || !description || !image || !nationality || !dob || !teams) {
-//       return res.status(404).send("Faltan datos para crear un driver");
-//     }
-
-//     const [newDriver, created] = await Driver.create({
-//       where: {
-//         name,
-//         surname,
-//         description,
-//         image,
-//         nationality,
-//         dob,
-//       },
-//       defaults: {
-//         name,
-//         surname,
-//         description,
-//         image,
-//         nationality,
-//         dob,
-//       },
-//     });
-
-//     if (!created) {
-//       return res.status(400).send("No se puede crear el nuevo Driver");
-//     }
-
-//     for (let teamName of teams) {
-//       const [teamInstance] = await Teams.findOrCreate({
-//         where: { name: teamName },
-//       });
-
-//       // Asegúrate de que "teamInstance" y "newDriver.id" sean válidos
-//       if (teamInstance && newDriver && newDriver.id) {
-//         await Drivers_Teams.create({
-//           teamId: teamInstance.id,
-//           typeId: newDriver.id,
-//         });
-//       } else {
-//         // Maneja el caso donde "teamInstance" o "newDriver.id" no son válidos
-//         return res.status(500).json({ mensaje: "Error al crear la relación entre conductor y equipo" });
-//       }
-//     }
-
-//     return res.status(200).json(newDriver);
-//   } catch (error) {
-//     console.error("Error en la creación del conductor:", error);
-//     return res.status(500).json({ mensaje: "Se produjo un error al crear el conductor" });
-//   }
-// };
-
-// module.exports = {
-//   postDrivers,
-// };
+  
