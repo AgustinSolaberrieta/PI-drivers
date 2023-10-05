65ed050dd5bb9eff668d8c01e6aa85f9244a0317
AgustinSolaberrieta <agustinasolaberrieta08@gmail.com>
2023-10-05 15:20:34 -0300

    tee

--- server/src/controller/getTeams.js
+++ server/src/controller/getTeams.js
@@ -4,52 +4,45 @@ En una primera instancia, cuando la base de datos este vacía, deberás guardar 
 Estos deben ser obtenidos de la API (se evaluará que no haya hardcodeo). Luego de obtenerlos de la API, deben ser guardados en la base de datos para su posterior consumo desde allí. */
 
 const { where } = require("sequelize");
-const {Teams}= require("../db");
+const { Teams } = require("../db");
 const axios = require("axios")
 const URL = "http://localhost:5000/drivers";
 
-const getTeams= async(req, res) =>{
+const getTeams = async (req, res) => {
     try {
-        // Me fijo si la base de datos está vacía
-       const teamsEnDB = await Teams.findAll();
-      //  flatmap
-       //si tiene algo me lo devuelve 
-       if (teamsEnDB.length > 0) {
-                return res.status(201).json(teamsEnDB)
+        const response = await axios(URL);
+        const mapeo = response.data.map(team => team.teams);
+
+        const newmapeo = mapeo.flatMap(element => {
+            if (element) {
+                let arry = element.split(',');
+                return arry;
+            } else {
+                return [];
             }
+        });
+        const newnewmapeo = newmapeo.map(elemento => elemento.trim());//el trim elimina los espacios en blanco de adelante y atras 
+        const arrayteams = [...new Set(newnewmapeo)];// el set es para eliminar elementos duplicados del nuevo array resultante
+ 
+        const listTeams = await Promise.all(
+            arrayteams.map(async (team)=>{
+                const [newteam] = await Teams.findOrCreate({
+                    where: {name: team}
+                })
+                return {
+                    id: newteam.id,
+                    name: newteam.name
+                }
+            })
+        )
 
-      const response = await axios(URL);    
-       const dataTeams = response.data;
-      
-        // console.log(typeof dataTeams);
-       const mapeo = dataTeams.map(team => team.teams)
-       const newmapeo= mapeo.flatMap(element =>{
-        if (element) {
-          let arry = element.split(',');
-          return arry;
-        } else {
-          return [];
-        }})
-        const newnewmapeo= newmapeo.flatMap(elemento => elemento.trim())
-
-       const equiposUnicos= [...new Set(newnewmapeo)]
-    //await Teams.bulkCreate(equiposUnicos.map(name => ({ name })));
-
-
-       // const gusrdoenbasededatos= await Teams.bulkCreate(equiposUnicos)
-     
-      // console.log(gusrdoenbasededatos);
-
-        return res.status(200).json(equiposUnicos)
-
-
+        return res.status(200).json(listTeams);
     } catch (error) {
         console.log(error.message);
-      return res.status(500).json({error: error.message})   
+        return res.status(500).json({ error: error.message });
     }
 }
 
-
-module.exports={
+module.exports = {
     getTeams,
 }
